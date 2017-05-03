db64fc3ccaae206d3dde1ff38117c494ca064087
17liamnaddell <liamnprg@gmail.com>
2017-05-03 00:15:26 -0400

    gpio support

--- src/main.py
+++ src/main.py
@@ -1,17 +1,15 @@
-import RPi.GPIO as GPIO  
-import time  
-# blinking function  
-def blink(pin):  
-        GPIO.output(pin,GPIO.HIGH)  
-        time.sleep(1)  
-        GPIO.output(pin,GPIO.LOW)  
-        time.sleep(1)  
-        return  
-# to use Raspberry Pi board pin numbers  
-GPIO.setmode(GPIO.BOARD)  
-# set up GPIO output channel  
-GPIO.setup(11, GPIO.OUT)  
-# blink GPIO17 50 times  
-for i in range(0,50):  
-        blink(11)  
-GPIO.cleanup()  
+import RPi.GPIO as GPIO, time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17,GPIO.OUT)
+
+def blinkOnce(pin):
+        GPIO.output(pin,True)
+        time.sleep(1)
+        GPIO.output(pin,False)
+        time.sleep(1)
+
+for i in range(10):
+        blinkOnce(17)
+
+GPIO.cleanup()
