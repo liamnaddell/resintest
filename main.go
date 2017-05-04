43276fbc20d670042a39f614ec704000ec23ffa7
17liamnaddell <liamnprg@gmail.com>
2017-05-04 08:53:31 -0400

    moved GPIO pins, broke resin.io

--- main.go
+++ main.go
@@ -1,28 +1,9 @@
 package main
 
 import (
-	"time"
-
-	"gobot.io/x/gobot"
-	"gobot.io/x/gobot/drivers/gpio"
-	"gobot.io/x/gobot/platforms/raspi"
+	"fmt"
 )
 
 func main() {
-	r := raspi.NewAdaptor()
-	led := gpio.NewLedDriver(r, "11")
-
-	work := func() {
-		gobot.Every(1*time.Second, func() {
-			led.Toggle()
-		})
-	}
-
-	robot := gobot.NewRobot("blinkBot",
-		[]gobot.Connection{r},
-		[]gobot.Device{led},
-		work,
-	)
-
-	robot.Start()
+	fmt.Println("hello world")
 }
